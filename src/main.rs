fcb4330d3109a4d8add1bd7f690a39b25e32aae8
Alexi Dg <alexisdurangomez588@gmail.com>
2026-07-21 15:39:09 -0500

    feat: :fire: ferris-use file has been added

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,3 @@
-/* fn main() {
-    let texto_multilinea = r#"
-    Esta es la primera línea.
-    Esta es la segunda línea.
-    Y esta es la tercera.
-    "#;
-    println!("{}", texto_multilinea);
-    println!(" ===== Hello, world! =====");
-    println!(r"< Hello fellow Rustaceans! >
- --------------------------
-        \
-         \
-            _~^~^~_
-        \) /  o o  \ (/
-          '_   -   _'
-          / '-----' \");
-}*/
-
-
-use ferris_says::say; // from the previous step
-use std::io::{stdout, BufWriter};
-
 fn main() {
-    let stdout = stdout();
-    let message = String::from("Hello fellow Rustaceans!");
-    let width = message.chars().count();
-
-    let mut writer = BufWriter::new(stdout.lock());
-    say(&message, width, &mut writer).unwrap();
+    println!("Hello fellow Rustaceans!");
 }
